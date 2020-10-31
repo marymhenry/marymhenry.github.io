0e16502c7ae530e4e284fb0c10065730abeca921
marymhenry <64865723+marymhenry@users.noreply.github.com>
2020-10-31 11:57:51 -0500

    Updated Folders

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+console.log ("Your index.js file has loaded correctly!");
+
+$(".menuItem").hover(
+  function(){
+  $(this).css("color", "black")
+}, function(){
+  $(this).css("color", "inherit")
+});
+
+
